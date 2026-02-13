d4f7b7ea3d7429ee1d8dc40a136db44970fd2899
Mucha-B <kierumucha@gmail.com>
2026-02-13 07:53:30 +0300

    FreeCodeCamp, 4 2026 Winter Games Day 6 Figure Skating, problem of the day solved

--- 5_Freecodecamp/1_Javascript/4 2026 Winter Games Day 6 Figure Skating.js
+++ 5_Freecodecamp/1_Javascript/4 2026 Winter Games Day 6 Figure Skating.js
@@ -16,26 +16,44 @@
 
 /*
     Pseudo
-    1. Look for smallest value in array
-    2. Look for largest value in array
-    3. Sum all values in array to get total
-    4. Subtract then smallest, largest, and the penalty values
+    1. Sort arrays
+    1. Get smallest value
+    2. Get largest value
+    3. Sum all values in scores and penalties array to get total
+    4. Subtract smallest, largest, and penalty from judgeScores
 
 */
 
 function computeScore(judgeScores, ...penalties) {
 
-    // var judgeScores = judgeScores;
-    // var largeNum = 0, smallNum = 0;
-    // console.log(judgeScores.length);
-    for(let i = 0; i < judgeScores.length; i++){
-        console.log(judgeScores[i]);
+    
+    // Sort array
+    var judgeScores = judgeScores.slice().sort(function(a, b) {return a - b;});
+    var largestVal = judgeScores[0];
+    var smallestVal = judgeScores[judgeScores.length-1];
+    var judgeScoresTotal = 0;
+    var penaltiesTotal = 0;
+
+    for(let i = 0; i < penalties.length; i++){
+        penaltiesTotal += penalties[i];
     }
 
-//   return judgeScores;
+    for(let j = 0; j < judgeScores.length; j++){
+        judgeScoresTotal += judgeScores[j];
+    }
+
+    judgeScores = judgeScoresTotal - (largestVal + smallestVal + penaltiesTotal);
+    
+    console.log(judgeScores);
+    
+
+    return judgeScores;
+
 }
 
 
-computeScore([10, 8, 9, 6, 9, 8, 8, 9, 7, 7], 1);
-
-// console.log(computeScore([10, 8, 9, 6, 9, 8, 8, 9, 7, 7], 1));
+// computeScore([10, 8, 9, 6, 9, 8, 8, 9, 7, 7], 1) // should return 64.
+// computeScore([10, 10, 10, 10, 10, 10, 10, 10, 10, 10]) // should return 80.
+// computeScore([10, 8, 9, 10, 9, 8, 8, 9, 10, 7], 1, 2, 1) // should return 67.
+// computeScore([8.0, 8.5, 9.0, 8.5, 9.0, 8.0, 9.0, 8.5, 9.0, 8.5], 0.5, 1.0) // should return 67.5.
+// computeScore([6.0, 8.5, 7.0, 9.0, 7.5, 8.0, 6.5, 9.5, 7.0, 8.0], 1.5, 0.5, 0.5) // should return 59.
--- 5_Freecodecamp/1_Javascript/tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-for(let i = 0; i < judgeScores.length; i++){
-        console.log(judgeScores[i]);
-    }
